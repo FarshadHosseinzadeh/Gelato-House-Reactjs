c3a06a3b89606f7cfb2af2ecd34d66ccd0e0934a
Ershad Hosseinzadeh <hosseinzadehershad@gmail.com>
2026-02-19 02:41:09 +0330

    fix sliders pics

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -11,32 +11,32 @@ const HomePage: React.FC = (): JSX.Element => {
         slides={[
           {
             id: 1,
-            src: "../../../public/assets/images/slide1.jpg",
+            src: "https://static.delino.com/Image/Subscriber/gelatohouse/Sliders/3tkdpavp.ops.jpg",
             alt: "slide1",
           },
           {
             id: 2,
-            src: "../../../public/assets/images/slide2.jpg",
+            src: "https://static.delino.com/Image/Subscriber/gelatohouse/Sliders/5fz4nj3i.351.jpg",
             alt: "slide2",
           },
           {
             id: 3,
-            src: "../../../public/assets/images/slide3.jpg",
+            src: "https://static.delino.com/Image/Subscriber/gelatohouse/Sliders/aqgt0y34.a0s.jpg",
             alt: "slide3",
           },
           {
             id: 4,
-            src: "../../../public/assets/images/slide4.jpg",
+            src: "https://static.delino.com/Image/Subscriber/gelatohouse/Sliders/jwgd5v40.nlg.jpg",
             alt: "slide4",
           },
           {
             id: 5,
-            src: "../../../public/assets/images/slide5.jpg",
+            src: "https://static.delino.com/Image/Subscriber/gelatohouse/Sliders/ltmlvita.22g.jpg",
             alt: "slide5",
           },
           {
             id: 6,
-            src: "../../../public/assets/images/slide6.jpg",
+            src: "https://static.delino.com/Image/Subscriber/gelatohouse/Sliders/ochrogbb.3vb.jpg",
             alt: "slide6",
           },
         ]}
