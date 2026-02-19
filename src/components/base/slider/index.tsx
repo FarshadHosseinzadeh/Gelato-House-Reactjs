0d579efeacce3d13812f96bcf22a13a74f58ffa7
Ershad Hosseinzadeh <hosseinzadehershad@gmail.com>
2026-02-19 12:53:15 +0330

    slider changes

--- src/components/base/slider/index.tsx
+++ src/components/base/slider/index.tsx
@@ -1,6 +1,13 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 import { Swiper, SwiperSlide, type SwiperProps } from "swiper/react";
+import { EffectFade, Navigation } from "swiper/modules";
+
 import "swiper/swiper.css";
+
+import "swiper/css/effect-fade";
+
+import "swiper/css/navigation";
+//
 
 type slides = {
   id: number;
@@ -13,17 +20,34 @@ type SliderProps = SwiperProps & {
 };
 
 const Slider: React.FC<SliderProps> = ({ slides, ...props }): JSX.Element => {
+  const [thumbsSwiper] = useState(null);
   return (
-    <Swiper {...props}>
-      {slides.map((slide) => (
-        <SwiperSlide
-          key={slide.id}
-          className="flex justify-center items-center"
-        >
-          <img src={slide.src} alt={slide.alt} width="auto" height="300px" />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative">
+      <button className="absolute left-2 top-1/2 z-10 -translate-y-1/2 bg-black px-3 py-2 text-white swiper-button-prev-custom">
+        Prev
+      </button>
+      <button className="absolute right-2 top-1/2 z-10 -translate-y-1/2 bg-black px-3 py-2 text-white swiper-button-next-custom">
+        Next
+      </button>
+      <Swiper
+        {...props}
+        modules={[EffectFade, Navigation]}
+        effect="fade"
+        thumbs={{ swiper: thumbsSwiper }}
+        navigation={{
+          prevEl: ".swiper-button-prev-custom",
+          nextEl: ".swiper-button-next-custom",
+        }}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.id}>
+            <div className="flex justify-center items-center">
+              <img src={slide.src} alt={slide.alt} width="80%" height="300px" />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
